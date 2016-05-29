3d2147e06cbd6e1a255e792d998bcc8f04863a50
pablovillegass <pablovillegas@opendeusto.es>
2016-05-30 01:41:20 +0200

    Add files via upload
    Clase vuelo con métodos básicos, falta implementación con BD.

---
+++ Vuelos/Vuelo.h
@@ -0,0 +1,54 @@
+#ifndef Vuelo_h
+#define Vuelo_h
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+    
+    class Trayecto
+    {
+    private:
+    
+        int cod_V;
+        std::string aero_Orig;
+        std::string aero_Dest;
+        int fecha;
+        int hora_Sal;
+        int hora_Lleg;
+        int precio;
+        
+    public:
+        Trayecto(int cod_V, std::string aero_Orig, std::string aero_Dest, int fecha, int hora_Sal, int hora_Lleg, int precio);
+        
+        ~Trayecto();
+        
+        int getcod_V() const;
+        std::string getAeroO() const;
+        std::string getAeroD() const;
+        int getFecha() const;
+        int getHoraS() const;
+        int getHoraL() const;
+        int getPre() const;
+    };
+    
+    class Avion
+    {
+    private:
+        
+        int cod_A;
+        std::string nom_compania;
+        int asientos;
+        
+    public:
+        
+        Avion(int cod_A, std::string nom_compania, int asientos);
+        
+        ~Avion();
+        
+        int getcod_A() const;
+        std::string getNomC() const;
+        int getAsiento() const;
+        
+    };
+
+#endif
